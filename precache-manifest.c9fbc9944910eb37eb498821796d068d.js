17ee41574243219efd4a4dc22332e1e1fab3fb3b
John Delta <john.deligiannis1@gmail.com>
2020-08-28 20:31:10 +0300

    Updates

---
+++ precache-manifest.c9fbc9944910eb37eb498821796d068d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cead1cbb9571f36a7e93a423af05877",
+    "url": "/biriba/index.html"
+  },
+  {
+    "revision": "4f4189bece96fa55b2fa",
+    "url": "/biriba/static/css/main.de9bf2e4.chunk.css"
+  },
+  {
+    "revision": "cfa4fd45eb8dcfde0107",
+    "url": "/biriba/static/js/2.6e21fddc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/biriba/static/js/2.6e21fddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4189bece96fa55b2fa",
+    "url": "/biriba/static/js/main.bb71eae1.chunk.js"
+  },
+  {
+    "revision": "f6858c69ed4ee0582800",
+    "url": "/biriba/static/js/runtime-main.930940ba.js"
+  },
+  {
+    "revision": "917b080efd13f5ebe64ab18c37b6ca89",
+    "url": "/biriba/static/media/background.917b080e.jpg"
+  }
+]);
